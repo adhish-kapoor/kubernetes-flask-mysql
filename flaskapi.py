719ba413a5b6fcb587ac2c41f212f27e94543ad8
Adhish Kapoor <adhish.kapoor@nagarro.com>
2023-06-10 22:18:05 +0530

    config changes

--- flaskapi.py
+++ flaskapi.py
@@ -1,4 +1,3 @@
-"""Code for a flask API to Create, Read, Update, Delete users"""
 import os
 from flask import jsonify, request, Flask
 from flaskext.mysql import MySQL
@@ -8,23 +7,28 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # MySQL configurations
-app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
-app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
-app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
-app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+# app.config["MYSQL_DATABASE_USER"] = "root"
+# app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
+# app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
+# app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
+# app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+MYSQL_DATABASE_USER = os.getenv('MYSQL_DATABASE_USER', 'root')
+MYSQL_DATABASE_PASSWORD = os.getenv('MYSQL_DATABASE_PASSWORD', 'admin')
+MYSQL_DATABASE_DB = os.getenv('MYSQL_DATABASE_DB', 'mysql')
+MYSQL_DATABASE_HOST = os.getenv('MYSQL_DATABASE_HOST', 'localhost')
+MYSQL_DATABASE_PORT = os.getenv('MYSQL_DATABASE_PORT', '3306')
 mysql.init_app(app)
 
 
+"""Function to test the functionality of the API"""
 @app.route("/")
 def index():
-    """Function to test the functionality of the API"""
-    return "Hello, world!"
+    return "Home Page"
 
 
+"""Function to create a user to the MySQL database"""
 @app.route("/create", methods=["POST"])
 def add_user():
-    """Function to create a user to the MySQL database"""
     json = request.json
     name = json["name"]
     email = json["email"]
@@ -49,9 +53,9 @@ def add_user():
         return jsonify("Please provide name, email and pwd")
 
 
+"""Function to retrieve all users from the MySQL database"""
 @app.route("/users", methods=["GET"])
 def users():
-    """Function to retrieve all users from the MySQL database"""
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
@@ -64,70 +68,6 @@ def users():
         return resp
     except Exception as exception:
         return jsonify(str(exception))
-
-
-@app.route("/user/<int:user_id>", methods=["GET"])
-def user(user_id):
-    """Function to get information of a specific user in the MSQL database"""
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE user_id=%s", user_id)
-        row = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        resp = jsonify(row)
-        resp.status_code = 200
-        return resp
-    except Exception as exception:
-        return jsonify(str(exception))
-
-
-@app.route("/update", methods=["POST"])
-def update_user():
-    """Function to update a user in the MYSQL database"""
-    json = request.json
-    name = json["name"]
-    email = json["email"]
-    pwd = json["pwd"]
-    user_id = json["user_id"]
-    if name and email and pwd and user_id and request.method == "POST":
-        # save edits
-        sql = "UPDATE users SET user_name=%s, user_email=%s, " \
-              "user_password=%s WHERE user_id=%s"
-        data = (name, email, pwd, user_id)
-        try:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sql, data)
-            conn.commit()
-            resp = jsonify("User updated successfully!")
-            resp.status_code = 200
-            cursor.close()
-            conn.close()
-            return resp
-        except Exception as exception:
-            return jsonify(str(exception))
-    else:
-        return jsonify("Please provide id, name, email and pwd")
-
-
-@app.route("/delete/<int:user_id>")
-def delete_user(user_id):
-    """Function to delete a user from the MySQL database"""
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM users WHERE user_id=%s", user_id)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        resp = jsonify("User deleted successfully!")
-        resp.status_code = 200
-        return resp
-    except Exception as exception:
-        return jsonify(str(exception))
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
